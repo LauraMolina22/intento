f4d0f8695b8eb7a9ad85b52b6dd8173c6ee16e0e
LauraMolina22 <lala.molina22@gmail.com>
2025-02-18 22:20:16 -0500

    curso

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Curso bases',
+  webDir: 'www'
+};
+
+export default config;
